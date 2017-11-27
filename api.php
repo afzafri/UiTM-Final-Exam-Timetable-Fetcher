da3067a52692cde84d14ad0f6641f9169522e642
afzafri <zafri_21596@yahoo.com>
2017-11-28 00:55:49 +0800

    get new redirected url

---
+++ api.php
@@ -0,0 +1,27 @@
+<?php 
+
+$baseurl = "http://istudent.uitm.edu.my/nsp/examttable";
+$dir = "/JWP_CURRENT_B/index.html";
+   
+// need to fetch the new redirected url first, this is because the url will change each semester
+$ch = curl_init(); # initialize curl object
+curl_setopt($ch, CURLOPT_URL, $baseurl.$dir); # set url
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); # receive server response
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); # disable SSL verification (THIS IS NOT PROPER/SAFE)
+$result = curl_exec($ch); # execute curl, fetch webpage content
+$httpstatus = curl_getinfo($ch, CURLINFO_HTTP_CODE); # receive http response status
+$err = curl_error($ch);
+curl_close($ch);  # close curl
+
+# use regex to to parse html and get the new redirected url
+$patern = '#window.location="..([\w\W]*?)";#';
+preg_match_all($patern, $result, $parsed); 
+$newdir = implode(str_replace(['window.location="..','";','index.html'], "", $parsed[0]),"");
+
+// new url
+$newbaseurl = $baseurl.$newdir;
+
+
+
+
+?>
